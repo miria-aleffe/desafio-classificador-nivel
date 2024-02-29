3a59a10da706608956112c8c109b133f87ea4e85
miria <miria_aleffe@hotmail.com>
2024-02-28 22:25:22 -0300

    finalizando mais um desafio

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ class Hero {
         this.nome = nome
         this.idade = idade
         this.classe = classe
-        this.ataque = ""
+        this.ataque = " "
 
-        if(classe = "Guerreiro"){
-            ataque = "espada"
-        }else if(classe = "Mago"){
-            ataque = "Magia"
+        if(classe === "Guerreiro"){
+            this.ataque = "espada"
+        }else if(classe === "Mago"){
+            this.ataque = "Magia"
         }
     }
     show(){
-        console.log("O" + this.nome + "atacou usando" + this.ataque )
+        console.log("O " + this.nome + " atacou usando " + this.ataque )
     }
 }
+ let Hero1 = new Hero("Miria", 26, "Guerreiro")
+ let Hero2 = new Hero("Maria", 15, "Mago")
+
+ Hero1.show()
+ Hero2.show()
